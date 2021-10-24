667a245279105ab8d9a2452ffe30f32c5b54d3ca
Sudharshan <sudharshanjavaguide@gmail.com>
2021-10-24 15:53:29 +0530

    Student Class Added With Properties and Constructor

---
+++ src/in/sudharshanit/bean/Student.java
@@ -0,0 +1,19 @@
+package in.sudharshanit.bean;
+
+public class Student {
+
+	private Integer studentId;
+	private String studentName;
+	private String collegeName;
+	private String branchName;
+	public Student(Integer studentId, String studentName, String collegeName, String branchName) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.collegeName = collegeName;
+		this.branchName = branchName;
+	}
+	
+	
+	
+}
