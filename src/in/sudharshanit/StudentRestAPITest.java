6a89fc0de193cd07852775f2c8297d7bd352de5e
Sudharshan <sudharshanjavaguide@gmail.com>
2021-10-24 14:03:16 +0530

    DisplayStudentDetails() Method Added

--- src/in/sudharshanit/StudentRestAPITest.java
+++ src/in/sudharshanit/StudentRestAPITest.java
@@ -1,6 +1,10 @@
 package in.sudharshanit;
 
 public class StudentRestAPITest {
+	
+	public void displayStudentDetails() {
+		System.out.println("Student Details Will Display here");
+	}
 	public static void main(String[] args) {
 		System.out.println("***Welcome To Student RestAPI***");
 	}
