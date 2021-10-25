cf760b9ea261560135fb8096d099d614d9926405
Sudharshan <sudharshanjavaguide@gmail.com>
2021-10-25 09:15:48 +0530

    College Class Added

---
+++ src/in/sudharshanit/college/College.java
@@ -0,0 +1,19 @@
+package in.sudharshanit.college;
+
+import java.util.List;
+
+public class College {
+	
+	private String collegeName;
+	private String collegeOwner;
+	private List<String> branches;
+	public College(String collegeName, String collegeOwner, List<String> branches) {
+		super();
+		this.collegeName = collegeName;
+		this.collegeOwner = collegeOwner;
+		this.branches = branches;
+	}
+	
+	
+
+}
